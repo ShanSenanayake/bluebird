a03efc8215b338a956960ba5a23e4f4c6e08b034
ShanSenanayake <senanayake92@gmail.com>
2015-10-06 13:20:04 +0200

    changed font style

--- awesome-settings/theme.lua
+++ awesome-settings/theme.lua
@@ -12,7 +12,7 @@ theme                               = {}
 theme.confdir                       = os.getenv("HOME") .. "/.config/awesome/themes/multicolor"
 theme.wallpaper                     = theme.confdir .. "/wall.png"
 
-theme.font                          = "Terminus 10"
+theme.font                          = "Ubuntu 10"
 --theme.taglist_font                =
 theme.menu_bg_normal                = "#000000"
 theme.menu_bg_focus                 = "#000000"
